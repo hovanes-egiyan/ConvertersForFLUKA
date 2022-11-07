c08cc738631a6ad097123a704d58ab7d30d36fb0
Hovanes <hovanes.egiyan@gmail.com>
2022-11-07 10:07:13 -0500

    Add files via upload

---
+++ Converter_FLUKA_BNN.py
@@ -0,0 +1,96 @@
+'''
+Created on Mar 24, 2022
+
+@author: Hovanes Egiyan
+'''
+
+import sys, string, numpy
+
+class Converter_FLUKA_BNN(object):
+    '''
+    CLASS to convert the text file output from FLUKA BNN binary file obtained with $FLUPRO/flutil/usbrea 
+    to a regular table in a text format. 
+    '''
+
+
+    def __init__(self ):
+        '''
+        Constructor
+        '''
+        self.inFileHandle = None 
+        self.outFileHandle = None
+        
+        self.numberOfHeaderLines = 8;
+        
+        
+        self.nX = 88
+        self.nY = 540 
+        self.nZ = 265 
+        
+        self.xMin = -2.2
+        self.xMax = +2.2
+        self.yMin = -13.5
+        self.yMax = +13.5
+        self.zMin = +78.
+        self.zMax = +131.
+        
+        self.data = numpy.zeros( (self.nX,self.nY,self.nZ) )
+        
+        return
+
+
+    def readFile(self, inFileName):
+        print "Opening file {0} for reading".format( inFileName)            
+        # Open input file
+        try :
+            self.inFileHandle = open( inFileName, "r" )
+        except IOError as err:
+            raise err
+        
+        lineNumber = 0
+        iData = 0
+
+        for inputLine in self.inFileHandle:
+            lineNumber += 1
+#            print lineNumber, " ", inputLine
+            if( lineNumber <= self.numberOfHeaderLines ) : 
+#                print " HEADER : " , inputLine
+                continue
+            vecElements = inputLine.split()
+            for data in vecElements:
+                iz = iData / ( self.nX * self.nY )
+                iy = ( iData % ( self.nX * self.nY ) ) / self.nX 
+                ix = ( iData % ( self.nX * self.nY ) ) % self.nX 
+                
+#                print "Point #{0} , ix={1}, iy={2}, iz={3}, value={4}".format(iData, ix, iy, iz, data )   
+                if( ix < self.nX and iy < self.nY and iz < self.nZ ) :            
+                    self.data[ix,iy,iz] = data 
+                iData += 1
+#                sys.stdout.write(data)
+#            print
+            
+        return 
+     
+    def writeFile(self, outFileName = None ):         
+        #Open output file
+        if( outFileName == None ) :
+            outFileName2Use = inFileName + ".table"
+        else :
+            outFileName2Use = outFileName 
+            
+        print "Opening file {0} for writing".format( outFileName2Use)    
+        try :
+            self.outFileHandle = open( outFileName2Use, "w" )
+        except IOError as err:
+            raise err
+        
+        for ix in range ( 0, self.nX ) :
+            for iy in range( 0, self.nY ) :
+                for iz in range( 0, self.nZ ) :
+                    x = self.xMin + (ix+0.5) * ( self.xMax - self.xMin ) / self.nX
+                    y = self.yMin + (iy+0.5) * ( self.yMax - self.yMin ) / self.nY
+                    z = self.zMin + (iz+0.5) * ( self.zMax - self.zMin ) / self.nZ
+                    self.outFileHandle.write( "{0}\t{1}\t{2}\t\t{3} \n".format( x, y, z, self.data[ix,iy,iz] ) )
+        
+        self.outFileHandle.close()
+            
---
+++ bnnText2Table.py
@@ -0,0 +1,86 @@
+#!/usr/bin/python2.7
+'''
+Created on Mar 24, 2022
+
+@author: Hovanes Egiyan
+'''
+
+
+import sys, string, getopt, os
+
+from optparse import OptionParser
+
+from Converter_FLUKA_BNN import Converter_FLUKA_BNN
+
+#===============================================================================
+# Class to handle command line options
+#===============================================================================
+class clfOptions:
+    """ Class to handle command line options  """
+        
+    # Constructor
+    def __init__( self, argList ):
+        self.optionDict = {}
+        # Command line
+        try:
+            # Get options from command line, may get overriden 
+            self.parseCommandLine( argList )  
+        except getopt.GetoptError as errMsg:
+            print errMsg
+            self.printHelpMessage()
+            sys.exit(-1)
+        return
+    
+    
+    # Parse command file and append the option list extracted from the command file
+    def parseCommandLine( self, argList ):
+        print "Starting command line parsing:", argList
+        # Here I am using optparse module which may be obsolete in future Python versions 
+        # This part might need to be redone for it to work with Python 3.1
+        # Python does not seem to be forward or backward compatible
+        parser = OptionParser(usage = "usage: %prog [options] ")
+        parser.add_option( "-1", "--f1", 
+                           action="store", dest="f1", type="string", metavar="InFile", 
+                           default="InFile.xml", help="Define input file name to be updated" )
+        parser.add_option( "-2", "--f2", 
+                           action="store", dest="f2", type="string", metavar="OutFile", 
+                           default="OutFile.xml", help="Define new output file" )
+        
+        (opts, args) = parser.parse_args( argList )
+        
+        # Assign the elements of the dictionary to the parsed option values
+        self.optionDict["InFile"]  = opts.f1
+        self.optionDict["OutFile"] = opts.f2
+        return
+        
+        
+        
+    # Return option for key optKey if exists, otherwise return None
+    def getOption(self, optKey):
+        if optKey in self.optionDict:
+            return self.optionDict[optKey]
+        else :
+            return None
+
+
+    # Returns the dictionary of all options
+    def getOptions(self):
+        return self.optionDict
+
+        
+if __name__ == '__main__':
+    
+    globOptions = clfOptions( sys.argv[1:] )
+
+    # Get the global options from the clfOptions class
+    progOpts = globOptions
+
+    inFileName = progOpts.getOption( "InFile" )
+    outFileName = progOpts.getOption( "OutFile" )
+    
+    print "Input file is {0} , output file is {1}".format(inFileName, outFileName )  
+    
+    converter = Converter_FLUKA_BNN(   )
+    converter.readFile( inFileName )
+    converter.writeFile(outFileName)
+    
